a2d9a2aaf1fd454648864e309157c01daa8094ca
nphtai <huutaimenst10@gmail.com>
2021-07-16 22:09:53 +0700

    Thêm UI của app manage

---
+++ app/src/main/java/com/example/myapplication/controller/UpdateMainItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class UpdateMainItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_main_item);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/controller/ViewMainItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class ViewMainItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_main_item);
+    }
+}
